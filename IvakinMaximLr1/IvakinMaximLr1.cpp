094596113767989450e879907c6306de9055d5bf
max-iva1 <maxim.ivakinn@gmail.com>
2025-10-30 13:10:01 +0300

    IvakinMaximLr1.cpp

--- IvakinMaximLr1/IvakinMaximLr1.cpp
+++ IvakinMaximLr1/IvakinMaximLr1.cpp
@@ -1,176 +1,146 @@
-﻿#include <iostream>
+#include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
-#define INPUT_LINE(in, str) getline(in>>std::ws, str); \
-      std::cerr << str << std::endl
-
-#define PRINT_PARAM(out, x) out<< #x << "=" << x << std::endl
-
-
 template <typename T>
-T GetCorrectNumber(T min, T max)
-{
+T GetCorrectNumber(T min, T max) {
     T x;
-    while ((std::cin >> x).fail() 
-        || std::cin.peek() != '\n' 
-        || x < min || x > max)  
-    {
-        std::cin.clear();
-        std::cin.ignore(10000, '\n');
-        std::cout << "Type number (" << min << "-" << max << "):";
+    while ((cin >> x).fail() || cin.peek() != '\n' || x < min || x > max) {
+        cin.clear();
+        cin.ignore(10000, '\n');
+        cout << "Vvedite chislo (" << min << "-" << max << "): ";
     }
-    std::cerr << x << std::endl;
     return x;
 }
 
 class Pipes {
 public:
     void add() {
-        cout << "Введите название трубы: ";
-        INPUT_LINE(cin, _name);
-        cout << "Введите размеры трубы (длину): ";
-        _length = GetCorrectNumber(0, 100000);
-        cout << "Введите размеры трубы (ширину): ";
-        _width = GetCorrectNumber(0, 100000);
-        cout << "Введите работает ли труба(1 - работает, 0 - в ремонте): ";
-        _properties = GetCorrectNumber(0, 1);
+        cout << "Vvedite nazvanie truby: ";
+        getline(cin >> ws, _name);
+        cout << "Dlina: "; _length = GetCorrectNumber(0, 100000);
+        cout << "Shirina: "; _width = GetCorrectNumber(0, 100000);
+        cout << "Rabotaet (1 - da, 0 - net): "; _properties = GetCorrectNumber(0, 1);
     }
+
     void View() {
-        if (_name.empty()) {
-            cout << "Труба не найдена, добавьте трубу" << endl;
-        }
-        else {
-            cout << "Название: " << _name << ", длина: " << _length << " ширина: " << _width << " работает ли труба (1 - работает, 0 - в ремонте): " << _properties << endl;
-        }
+        if (_name.empty()) cout << "Truba ne naidena" << endl;
+        else cout << "Truba: " << _name << ", dlina: " << _length << ", shirina: " << _width
+            << ", status: " << (_properties ? "rabotaet" : "ne rabotaet") << endl;
     }
+
     void Edit() {
-        if (_name.empty()) {
-            cout << "Труба не найдена, добавьте трубу" << endl;
+        if (!_name.empty()) {
+            cout << "Rabotaet (1 - da, 0 - net): "; _properties = GetCorrectNumber(0, 1);
         }
-        else {
-            cout << "Введите работает ли труба(1 - работает, 0 - в ремонте)" << endl;
-            _properties = GetCorrectNumber(0, 1); 
-        }
+        else cout << "Truba ne naidena" << endl;
     }
-    void output_file() {
-        if (_name.empty()) {
-            cout << "Труба не найдена, добавьте трубу" << endl;
-        }
-        else {
-            ofstream out;
-            out.open("data_pipes_ks.txt");
-            if (out.is_open()) {
-                out << "Название: " << _name << ", длина: " << _length << " ширина: " << _width << " работает ли труба (1 - работает, 0 - в ремонте): " << _properties << endl;
-                out.close();
-            }
-        }
+
+    void saveToFile(ofstream& out) {
+        if (!_name.empty()) out << "PIPE:" << _name << "," << _length << "," << _width << "," << _properties << endl;
     }
-    void input_file() {
-        ifstream in;
-        in.open("data_pipes_ks.txt");
-        string line;
-        if (in.is_open()) {
-            while (getline(in, line)) {
-                cout << line << " ";
-            }
-            cout << endl;
-            in.close();
+
+    bool loadFromFile(const string& data) {
+        if (data.find("PIPE:") == 0) {
+            stringstream ss(data.substr(5));
+            getline(ss, _name, ',');
+            string temp;
+            getline(ss, temp, ','); _length = stoi(temp);
+            getline(ss, temp, ','); _width = stoi(temp);
+            getline(ss, temp); _properties = stoi(temp);
+            return true;
         }
+        return false;
     }
+
 private:
     string _name;
-    int _length;
-    int _width;
+    int _length, _width;
     bool _properties;
 };
 
 class KS {
 public:
     void add() {
-        cout << "Введите название КС: ";
-        INPUT_LINE(cin, _name);
-        cout << "Введите количество цехов: ";
-        _quantity = GetCorrectNumber(0, 100000);
-        cout << "Введите количество цехов в работе: ";
-        _count_work = GetCorrectNumber(0, _quantity); 
-        cout << "Введите запуск КС(1 - запустить, 0 - остановка): ";
-        _station_class = GetCorrectNumber(0, 1);
+        cout << "Vvedite nazvanie KS: ";
+        getline(cin >> ws, _name);
+        cout << "Kolichestvo tsehov: "; _quantity = GetCorrectNumber(0, 100000);
+        cout << "Tsehov v rabote: "; _count_work = GetCorrectNumber(0, _quantity);
+        cout << "Rabotaet (1 - da, 0 - net): "; _station_class = GetCorrectNumber(0, 1);
     }
+
     void View() {
-        if (_name.empty()) {
-            cout << "КС не найден, добавьте КС" << endl;
-        }
-        else {
-            cout << "Название: " << _name << ", количество цехов: " << _quantity << " количество цехов в работе: " << _count_work << " класс станции: " << _station_class << endl;
-        }
+        if (_name.empty()) cout << "KS ne naiden" << endl;
+        else cout << "KS: " << _name << ", tsehov: " << _quantity << ", rabotaet: " << _count_work
+            << ", status: " << (_station_class ? "rabotaet" : "ne rabotaet") << endl;
     }
+
     void Edit() {
-        if (_name.empty()) {
-            cout << "КС не найден, добавьте КС" << endl;
+        if (!_name.empty()) {
+            cout << "Rabotaet (1 - da, 0 - net): "; _station_class = GetCorrectNumber(0, 1);
         }
-        else {
-            cout << "Введите запуск КС(1 - запустить, 0 - остановка)" << endl;
-            _station_class = GetCorrectNumber(0, 1); 
-        }
+        else cout << "KS ne naiden" << endl;
     }
-    void output_file() {
-        if (_name.empty()) {
-            cout << "КС не найден, добавьте КС" << endl;
-        }
-        else {
-            ofstream out("data_pipes_ks.txt", ios::app);
-            if (out.is_open()) {
-                out << "Название: " << _name << ", количество цехов: " << _quantity << " количество цехов в работе: " << _count_work << " класс станции: " << _station_class << '\n';
-                out.close();
-            }
-        }
+
+    void saveToFile(ofstream& out) {
+        if (!_name.empty()) out << "KS:" << _name << "," << _quantity << "," << _count_work << "," << _station_class << endl;
     }
+
+    bool loadFromFile(const string& data) {
+        if (data.find("KS:") == 0) {
+            stringstream ss(data.substr(3));
+            getline(ss, _name, ',');
+            string temp;
+            getline(ss, temp, ','); _quantity = stoi(temp);
+            getline(ss, temp, ','); _count_work = stoi(temp);
+            getline(ss, temp); _station_class = stoi(temp);
+            return true;
+        }
+        return false;
+    }
+
 private:
     string _name;
-    int _quantity;
-    int _count_work;
+    int _quantity, _count_work;
     bool _station_class;
 };
 
-int main()
-{
-    setlocale(LC_ALL, "Russian");
-    Pipes pipe_;
-    KS KS_;
+int main() {
+    Pipes pipe;
+    KS ks;
+
     while (true) {
-        cout << "\n1. Добавить трубу\n2. Добавить КС\n3. Просмотр\n4. Редактировать трубу\n"
-            << "5. Редактировать КС\n6. Сохранить\n7. Загрузить\n0. Выход\nВыберете : ";
+        cout << "\n1. Dobavit trubu\n2. Dobavit KS\n3. Prosmotr\n4. Redaktirovat trubu\n"
+            << "5. Redaktirovat KS\n6. Sohranit\n7. Zagruzit\n0. Vihod\nVyberite: ";
+
         switch (GetCorrectNumber(0, 7)) {
-        case 0:
-            cout << "Вы закончили цикл" << endl;
-            exit(0);
-        case 1:
-            pipe_.add();
-            break;
-        case 2:
-            KS_.add();
-            break;
-        case 3:
-            pipe_.View();
-            KS_.View();
-            break;
-        case 4:
-            pipe_.Edit();
-            break;
-        case 5:
-            KS_.Edit();
-            break;
-        case 6:
-            pipe_.output_file();
-            KS_.output_file();
-            break;
-        case 7:
-            pipe_.input_file();
+        case 0: return 0;
+        case 1: pipe.add(); break;
+        case 2: ks.add(); break;
+        case 3: pipe.View(); ks.View(); break;
+        case 4: pipe.Edit(); break;
+        case 5: ks.Edit(); break;
+        case 6: {
+            ofstream out("data.txt");
+            pipe.saveToFile(out);
+            ks.saveToFile(out);
+            cout << "Dannye sohraneny" << endl;
             break;
         }
+        case 7: {
+            ifstream in("data.txt");
+            string line;
+            while (getline(in, line)) {
+                if (!pipe.loadFromFile(line) && !ks.loadFromFile(line)) {
+                    cout << "Neznany format: " << line << endl;
+                }
+            }
+            cout << "Dannye zagruzheny" << endl;
+            break;
+        }
+        }
     }
-    return 0;
 }
